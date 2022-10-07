c7bc37a9264600c7607100f0ff1a1a58bb2c5088
Renaud Deschenes <102036597+ZelenskyGigaChad@users.noreply.github.com>
2022-10-07 18:23:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -33,13 +33,16 @@ def jeu_de_devinettes():
                essaie += 1
 #si c'est le bon nombre, on lui dis son nombre d'essaies et on lui demande si il veut rejouer
            else:
+               essaie += 1
                print("Game Over! Le nombre etait", the_number, ". Vous avez reussit en ",essaie,"essaies")
                jouer_encore = input("Voulez-vous rejouer? O ou N : ").lower()
 #si c'est non on quitte
            if jouer_encore == "n":
+               print("Au revoir")
                jouer = False
 #si c'est oui on rejoue
            elif jouer_encore == "o":
+               les_limites()
                jeu_de_devinettes()
 #on appelle les deux fonctions dans le bon ordre
 les_limites()
