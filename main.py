4033ca079ce3293147ddff3e6a8b4e6dd55b9d52
Deschenesr <deschenesr@educ.college-montreal.qc.ca>
2022-10-12 08:14:35 -0400

    second commit

--- main.py
+++ main.py
@@ -1,49 +1,66 @@
-#on importe les librairies requise
+# on importe les librairies requise
 import random
-#on definie la limite initiale
-limites = "100"
-#on cree une fonction pour le choix de la borne
-def les_limites():
-    global limites
-#on demande la borne
-    limites = input("Quelle est la limite de nombre que vous voulez : ")
-#on retourne la variable
-    return limites
 
-#on cree une variable pour le jeu de devinette
+# on definie la limite initiale
+limites1 = "1"
+limites2 = "100"
+
+
+# on cree une fonction pour le choix de la borne
+def les_limites():
+    global limites2
+    global limites1
+
+    # on demande la borne
+    limites1 = input("Quelle est la limite de nombre que vous voulez : ")
+    limites2 = input("Quelle est la limite de nombre que vous voulez : ")
+    # on retourne la variable
+    return limites2
+    return limites1
+
+
+# on cree une variable pour le jeu de devinette
 def jeu_de_devinettes():
-#on genere le nombre
-    the_number = random.randint(1, int(limites))
+    # on genere le nombre
+    the_number = random.randint(int(limites1), int(limites2))
     guess = 0
     essaie = 0
     jouer_encore = ""
     jouer = True
-#on cree le jeu avec les conditions requise pour activer le jeu
+    # on cree le jeu avec les conditions requise pour activer le jeu
     while jouer == True:
         while guess != the_number:
-#on demande le guess
-           guess = int(input("Entrez un nombre svp : "))
-#si il est plus petit on le fait retenter sa chance en ajoutant 1 essaies
-           if guess > the_number:
-               print("Plus petit")
-               essaie += 1
-#si il est plus grand on le fait retenter sa chance en ajoutant 1 essaies
-           elif guess < the_number:
-               print("Plus grand")
-               essaie += 1
-#si c'est le bon nombre, on lui dis son nombre d'essaies et on lui demande si il veut rejouer
-           else:
-               essaie += 1
-               print("Game Over! Le nombre etait", the_number, ". Vous avez reussit en ",essaie,"essaies")
-               jouer_encore = input("Voulez-vous rejouer? O ou N : ").lower()
-#si c'est non on quitte
-           if jouer_encore == "n":
-               print("Au revoir")
-               jouer = False
-#si c'est oui on rejoue
-           elif jouer_encore == "o":
-               les_limites()
-               jeu_de_devinettes()
-#on appelle les deux fonctions dans le bon ordre
-les_limites()
+            # on demande le guess
+            guess = int(input("Entrez un nombre svp : "))
+            # si il est plus petit on le fait retenter sa chance en ajoutant 1 essaies
+            if guess > the_number:
+                print("Plus petit")
+                essaie += 1
+            # si il est plus grand on le fait retenter sa chance en ajoutant 1 essaies
+            elif guess < the_number:
+                print("Plus grand")
+                essaie += 1
+            # si c'est le bon nombre, on lui dis son nombre d'essaies et on lui demande si il veut rejouer
+            else:
+                essaie += 1
+                print("Game Over! Le nombre etait", the_number, ". Vous avez reussit en ", essaie, "essaies")
+                jouer_encore = input("Voulez-vous rejouer? O ou N : ").lower()
+            # si c'est non on quitte
+            if jouer_encore == "n":
+                print("Au revoir")
+                jouer = False
+            # si c'est oui on rejoue
+            elif jouer_encore == "o":
+                demande()
+                jeu_de_devinettes()
+
+
+# on appelle les deux fonctions dans le bon ordre
+def demande():
+    demande = input("Voulez-vous choisir les bornes? O / N : ").lower()
+    if demande == "o":
+        les_limites()
+
+
+demande()
 jeu_de_devinettes()
